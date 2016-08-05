bf0e35ec42acbf6bbb8470068e28cdff87e55188
andela-tadesanya <adeyemi.adesanya@andela.com>
2016-08-05 16:28:36 +0100

    [#127152289] link company model to user profile model

--- common/djangoapps/student/submodels/company.py
+++ common/djangoapps/student/submodels/company.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from student.models import UserProfile
 
 
 class UserCompany(models.Model):
     '''Holds the company field for each student user profile'''
-    user = models.OneToOneField(User, unique=True, db_index=True, related_name='company')
-    company = models.CharField(blank=True, max_length=50, db_index=True)
+    userprofile = models.OneToOneField(UserProfile, on_delete=models.CASCADE, primary_key=True)
+    company = models.CharField(blank=True, null=True, max_length=50, db_index=True)
 
     class Meta(object):
         db_table = "auth_usercompany"
